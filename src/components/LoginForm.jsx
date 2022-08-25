3f1f71e6e25bb87c078bc5df201557e0b94b09d2
nicolofabris <nicolo.fabriss@gmail.com>
2022-08-25 14:36:26 +0200

    frontend

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -22,7 +22,7 @@ export default function LoginForm({setLogin, setSignUp}) {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    axios.post("http://backend-dci-1.herokuapp.com/user/login", userInput)
+    axios.post("/user/login", userInput)
       .then((response) => {
         
         if(response.data.login){
